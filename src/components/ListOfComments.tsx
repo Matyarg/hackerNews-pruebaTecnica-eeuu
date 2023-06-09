c4e541c5627455d100c585800e2db490a5c38e00
matyarg <matias.g15@hotmail.com>
2023-06-08 22:56:25 -0300

    finalizando anidado de comentarios, faltan detalles

---
+++ src/components/ListOfComments.tsx
@@ -0,0 +1,51 @@
+import useSWR from 'swr'
+import { getItemInfo } from '../services/hacker-news'
+const Comment = (props: {
+    id: number
+}) => {
+    const { id } = props
+    const { data, isLoading } = useSWR(`/comment/${id}`, () => getItemInfo(Number(id)))
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+
+    const { by, text, time, kids } = data
+
+
+
+    return (
+        <>
+            <article>
+                <header>
+                    <small>
+                        <span>{by}</span>
+                        <span> - </span>
+                        <span>{time}</span>
+                    </small>
+                </header>
+                <p>{text}</p>
+            </article>
+            {
+                kids?.length > 0 && <ListOfComments ids={kids.slice(0, 10)} />
+            }
+        </>
+    )
+}
+
+export const ListOfComments = (props: {
+    ids: number[]
+}) => {
+    const { ids } = props
+    return (
+        <ul>
+            {
+                ids?.map((id: number) => (
+                    <li key={id}>
+                        <Comment id={id}></Comment>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,22 @@
-export default function DetailPage() {
+import useSWR from 'swr'
+import { getItemInfo } from '../services/hacker-news'
+import { ListOfComments } from '../components/ListOfComments'
+
+export default function DetailPage(props: {
+    params: {
+        id: string
+    }
+}) {
+    const { params: { id } } = props
+    const { data, isLoading } = useSWR(`/story/${id}`, () => getItemInfo(Number(id)))
+
+    const commentIds = data?.kids.slice(0, 10) ?? []
+
     return (
-        <h1>Detail</h1>
+        <div>
+            {
+                isLoading ? <p> Loading... </p> : <ListOfComments ids={commentIds} />
+            }
+        </div>
     )
 }
