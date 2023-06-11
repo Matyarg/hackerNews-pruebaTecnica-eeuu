9db8137ab65d5f9ec263ba81c07f4a5ebae967fb
matyarg <matias.g15@hotmail.com>
2023-06-10 22:30:44 -0300

    infinity scroll con SWR infinite

--- src/pages/TopStories.tsx
+++ src/pages/TopStories.tsx
@@ -1,20 +1,34 @@
-import useSWR from 'swr'
+// import useSWR from 'swr'
+import useSWRInfinite from 'swr/infinite'
+
 import { getTopStories } from '../services/hacker-news'
 import { Story } from "../components/Story";
 export default function TopStoriesPage() {
 
-    const { data, isLoading } = useSWR('stories', () => getTopStories(1, 6))
+    // const { data, isLoading } = useSWR('stories', () => getTopStories(1, 6))
+    const { data, isLoading, size, setSize } = useSWRInfinite(
+        (index) => `stories/${index + 1}`,
+        (key) => {
+            const [, page] = key.split('/')
+            return getTopStories(Number(page), 5)
+        }
+    )
+    const stories = data?.flat()
+
 
     return (
         <>
             <ul style={{ listStyle: 'none' }}>
-                {isLoading}
-                {data?.map((id: number, index: number) => (
+                {stories?.map((id: number, index: number) => (
                     <li key={id}>
                         <Story id={id} index={index} />
                     </li>
                 ))}
             </ul>
+
+            <button onClick={() => setSize(size + 1)}>
+                Load more
+            </button>
         </>
     )
 }
